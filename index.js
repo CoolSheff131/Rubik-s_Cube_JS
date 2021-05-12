61dba76b614e5d1a3681fe145e8d6ae4ea9cc7b7
CoolSheff131 <a.nikulsheev@gmail.com>
2021-05-12 22:17:45 +0800

    Добавлено создание кубика Рубика

--- index.js
+++ index.js
@@ -1,21 +1,79 @@
-class Cube{
-    constructor(N){
-        let sides = [];
+class Cube {
 
-        for (let i = 0; i < N; i++) { 
-            let arr = [[]]
-            for (let j = 0; j < N; j++) { 
-                arr[i][j] = i
-            }
-            sides.push(arr)
+  constructor(N) {
+    this.sides = [];
+    this.N = N
+    for (let i = 0; i < 6; i++) {
+      let side = [];
+      for (let j = 0; j < N; j++) {
+        let line = [];
+        for (let k = 0; k < N; k++) {
+          line.push(i);
         }
-
+        side.push(line);
+      }
+      this.sides.push(side);
     }
+    
+  }
+
+
 }
-let a = 1
-let element = document.getElementById("test")
-let chislo = document.getElementById("chislo")
-element.addEventListener("click", e=>{
-    chislo.innerHTML = a
-    a++;
+
+
+let a = 1;
+let spawnCube = document.getElementById("SpawnCube")
+colors = ["Red", "White", "Blue", "Green", "Yellow", "Orange"]
+spawnCube.addEventListener("click", e => {
+  let cubeSize = document.getElementById("cubeSize").value
+  cube = new Cube(cubeSize)
+  for (let i = 0; i < 6; i++) {
+    let side = document.getElementById("cube_side_" + i);
+    side.setAttribute("style",
+      "grid-template-columns: repeat(" + cube.N + ", 1fr);    grid-template-rows: repeat(" + cube.N + ", 1fr);"
+    )
+    for (let j = 0; j < cube.N; j++) {
+      for (let k = 0; k < cube.N; k++) {
+        let div = document.createElement("div");
+        div.style.background = colors[i];
+        div.classList.add("color")
+        side.appendChild(div);
+      }
+    }
+  }
+})
+let element = document.getElementById("test");
+let chislo = document.getElementById("chislo");
+let F = document.getElementById("F");
+let _F = document.getElementById("F'");
+let R = document.getElementById("R");
+let _R = document.getElementById("R'");
+let U = document.getElementById("U");
+let _U = document.getElementById("U'");
+let B = document.getElementById("B");
+let _B = document.getElementById("B'");
+let L = document.getElementById("L");
+let _L = document.getElementById("L'");
+let D = document.getElementById("D");
+let _D = document.getElementById("D'");
+
+F.addEventListener("click", (e) => {
+
 });
+_F.addEventListener("click", (e) => { });
+R.addEventListener("click", (e) => { });
+_R.addEventListener("click", (e) => { });
+U.addEventListener("click", (e) => { });
+_U.addEventListener("click", (e) => { });
+B.addEventListener("click", (e) => { });
+_B.addEventListener("click", (e) => { });
+L.addEventListener("click", (e) => { });
+_L.addEventListener("click", (e) => { });
+D.addEventListener("click", (e) => {
+  alert("1");
+});
+_D.addEventListener("click", (e) => {
+  alert("asd");
+});
+
+
