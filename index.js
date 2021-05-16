0a3a3f04ad604a98115fc17e31764b1abb55ad44
CoolSheff131 <a.nikulsheev@gmail.com>
2021-05-16 17:49:23 +0800

    исправлена проверка собранности одной из сторон кубика

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class Cube {
 
-  constructor(number,N) {
+  constructor(number, N) {
     this.number = number
     this.sides = [];
     this.N = N
@@ -20,7 +20,6 @@ class Cube {
   rotate(side, inverted, row) {
     let h = this.N - row - 1
     let temp = []
-
     if (row == 0) {
       let sideTemp = []
       for (let i = 0; i < this.N; i++) {
@@ -136,21 +135,21 @@ class Cube {
       }
     }
   }
-  getHTMLCube(){
+  getHTMLCube() {
     let divCube = document.createElement("div");
     divCube.classList.add("cube")
     let invisibleDivs = []
-    for(let i = 0;i<4;i++){
+    for (let i = 0; i < 4; i++) {
       let invisible = document.createElement("div")
       invisible.classList.add("invisible_side")
       invisibleDivs.push(invisible)
     }
     divCube.appendChild(invisibleDivs[0])
     this.divSides = []
-    for(let i = 0;i<6;i++){
+    for (let i = 0; i < 6; i++) {
       let divSide = document.createElement("div")
       divSide.classList.add("side")
-      divSide.setAttribute("style",        "grid-template-columns: repeat(" + cube.N + ", 1fr);    grid-template-rows: repeat(" + cube.N + ", 1fr);")
+      divSide.setAttribute("style", "grid-template-columns: repeat(" + cube.N + ", 1fr);    grid-template-rows: repeat(" + cube.N + ", 1fr);")
       for (let j = 0; j < cube.N; j++) {
         for (let k = 0; k < cube.N; k++) {
           let div = document.createElement("div");
@@ -159,44 +158,44 @@ class Cube {
           divSide.appendChild(div);
         }
       }
-      divSide.id = "cube"+this.number+"_side_"+i
+      divSide.id = "cube" + this.number + "_side_" + i
       this.divSides.push(divSide)
     }
     divCube.appendChild(this.divSides[0])
     divCube.appendChild(invisibleDivs[1])
     divCube.appendChild(invisibleDivs[2])
-    for(let i = 1;i<5;i++)
-    divCube.appendChild(this.divSides[i])
+    for (let i = 1; i < 5; i++)
+      divCube.appendChild(this.divSides[i])
     divCube.appendChild(invisibleDivs[3])
     divCube.appendChild(this.divSides[5])
     return divCube
   }
-  getHTMLButtons(){  //Создаем кнопки поворота сторон кубика N * N
-  let btns = document.createElement("div")
-  let row1 = document.createElement("div")
-  let row2 = document.createElement("div")
-  btns.appendChild(row1)
-  btns.appendChild(row2)
-  for (let j = 0; j < 6; j++) {
-    for (let i = 0; i < Math.floor(this.N / 2); i++) {
-      let button = document.createElement("button");
-      button.addEventListener("click", (e) => { this.rotate(j, false, i) })
-      button.classList.add("button")
-      button.textContent = (i + 1) + letters[j];
-      row1.appendChild(button)
-    }
+  getHTMLButtons() {  //Создаем кнопки поворота сторон кубика N * N
+    let btns = document.createElement("div")
+    let row1 = document.createElement("div")
+    let row2 = document.createElement("div")
+    btns.appendChild(row1)
+    btns.appendChild(row2)
+    for (let j = 0; j < 6; j++) {
+      for (let i = 0; i < Math.floor(this.N / 2); i++) {
+        let button = document.createElement("button");
+        button.addEventListener("click", (e) => { this.rotate(j, false, i) })
+        button.classList.add("button")
+        button.textContent = (i + 1) + letters[j];
+        row1.appendChild(button)
+      }
 
-  }
-  for (let j = 0; j < 6; j++) {
-    for (let i = 0; i < Math.floor(this.N / 2); i++) {
-      let button = document.createElement("button");
-      button.addEventListener("click", (e) => { this.rotate(j, true, i) })
-      button.classList.add("button")
-      button.textContent = (i + 1) + letters[j] + "'";
-      row2.appendChild(button)
     }
-  }
-  return btns
+    for (let j = 0; j < 6; j++) {
+      for (let i = 0; i < Math.floor(this.N / 2); i++) {
+        let button = document.createElement("button");
+        button.addEventListener("click", (e) => { this.rotate(j, true, i) })
+        button.classList.add("button")
+        button.textContent = (i + 1) + letters[j] + "'";
+        row2.appendChild(button)
+      }
+    }
+    return btns
   }
 
   visualize() {
@@ -213,24 +212,34 @@ class Cube {
   }
 }
 
-class Solver{
-  constructor(){
+class Solver {
+  constructor() {
     this.bestMoves = []
     this.solved = false
     this.moves = []
   }
-  solveOneSide(cubes,side,maxMoves) {
+  solveOneSide(cubes, maxMoves) {
     this.cubes = cubes
     this.cube = cubes[0]
-    this.side = side
-    let valuesMove = [] 
-    let i = 0
-    while (i < maxMoves && !this.solved) {
-      i++
+
+    let valuesMove = []
+    let countMoves = 0
+    let repeat = false
+    this.checkSolved()
+    if (this.solved) {
+      alert("Одна из сторон каждого кубика уже собрана")
+      return
+    }
+    while (countMoves < maxMoves && !this.solved) {
+      countMoves++
+      if (repeat || countMoves % 3 == 0) {
+        this.prevMove = this.bestMoves[this.bestMoves.length - 2]
+        repeat = true
+      }
       for (let j = 0; !this.solved && j < 6; j++) {
         for (let i = 0; i < !this.solved && Math.floor(this.cube.N / 2); i++) {
-          valuesMove.push(this.checkMove(j,true,i))
-          valuesMove.push(this.checkMove(j,false,i)) 
+          valuesMove.push(this.checkMove(j, true, i))
+          valuesMove.push(this.checkMove(j, false, i))
         }
       }
       let bestMove = this.moves[this.indexOfMax(valuesMove)]//берем самый ценный поворот
@@ -241,31 +250,51 @@ class Solver{
       valuesMove = []
       this.moves = []
     }
-    
-    if(this.solved){
-      alert((+side + 1)+" сторона собрана за " + i + " поворотов")
-    }else{
-      alert("Мы не смогли собрать "+(+side + 1)+" сторону за "+maxMoves+" поворотов :(")
-    }
     this.cube.visualize()
+    if (this.solved) {
+      alert("Одна из сторон собрана за " + countMoves + " поворотов")
+    } else {
+      alert("Мы не смогли собрать одну из сторон за " + maxMoves + " поворотов :(")
+    }
+    
   }
-  checkMove(j,inverted,i){
-    let move = [j,inverted,i]           //запоминаем поворот
-    this.moves.push(move)
-    this.cubes.forEach(curCube => {
-        curCube.rotate(j, inverted, i)
-      });
-    let values = this.checkSide() //вычисляем ценность поворота
+  checkMove(j, inverted, i) {
+    this.moves.push([j, inverted, i])//запоминаем поворот
+    if (this.prevMove == null || this.prevMove[0] != j || this.prevMove[1] != inverted || this.prevMove[2] != i) {
+      this.cubes.forEach(curCube => { curCube.rotate(j, inverted, i) });
+      let values = this.checkSolved()
+      this.cubes.forEach(curCube => { curCube.rotate(j, !inverted, i) });//поварачиваем обратно после проверки
+      return values
+    } else {//если поворот повторяется вернуть 0
+      let zero = []
+      for (let i = 0; i < 6 * this.cubes.length; i++) {
+        zero.push(0)
+      }
+      return zero;
+    }
+  }
+  checkSolved() {
+    let cubesValues = this.checkAllCubesSides()
     this.solved = true
-    values.forEach(value=>{
-        if (value != this.cube.N * this.cube.N) {//проверка собранности
-          this.solved = false
+    let isCubesSolved = []
+    for(let i = 0; i <this.cubes.length;i++){
+      isCubesSolved.push(false)
+    }
+    let curCube = 0
+    cubesValues.forEach(cubeValues => {
+      cubeValues.forEach(sideValue =>{
+        if(!isCubesSolved[curCube] && sideValue == this.cube.N * this.cube.N){
+          isCubesSolved[curCube] = true
+          console.log("solved")
         }
       })
-    this.cubes.forEach(curCube => {//поварачиваем обратно после проверки
-      curCube.rotate(j, !inverted, i)
-    });
-    return values
+      curCube++;
+    })
+    console.log(isCubesSolved)
+    isCubesSolved.forEach(isCubeSolved=>{
+      this.solved = this.solved && isCubeSolved
+    })
+    return cubesValues
   }
   indexOfMax(arr) {
     if (arr.length === 0) {
@@ -282,18 +311,23 @@ class Solver{
     return maxIndex;
   }
 
-  checkSide(){
+  checkAllCubesSides() {//узнать количество одинаковых цветов на каждой из сторон кубика
     let colorsValues = []
+    let allCubesValues =[]
     this.cubes.forEach(curCube => {//проверить собрана ли хотя бы одна сторона у одного куба
-      let colorsValue = [0,0,0,0,0,0]
-      for(let i = 0; i< this.cube.N;i++){
-        for(let j = 0;j< this.cube.N;j++){
-           colorsValue[curCube.sides[this.side][i][j]]++
+      for (let k = 0; k < 6; k++) {
+        let colorsValue = [0, 0, 0, 0, 0, 0]//считаем цвет которого больше всего на стороне
+        for (let i = 0; i < this.cube.N; i++) {
+          for (let j = 0; j < this.cube.N; j++) {
+            colorsValue[curCube.sides[k][i][j]]++
+          }
         }
+        colorsValues.push(Math.max.apply(null, colorsValue))
       }
-      colorsValues.push(Math.max.apply(null,colorsValue))
+      allCubesValues.push(colorsValues)
+      colorsValues = []
     });
-    return colorsValues
+    return allCubesValues
   }
 }
 let a = 1;
@@ -306,21 +340,21 @@ spawnCube.addEventListener("click", e => {
   document.getElementById("settingsID").hidden = true
   document.getElementById("solutionSettings").hidden = false
   let cubes = []
-  for(let i = 0;i<cubeNumber;i++){
-  cube = new Cube( 0, cubeSize)  
-  cubes.push(cube)
-  let cubeCard = document.createElement("div")
-  document.getElementById("cubes").appendChild(cubeCard)
-  cubeCard.appendChild(cube.getHTMLCube())
-  cubeCard.appendChild(cube.getHTMLButtons())
+  for (let i = 0; i < cubeNumber; i++) {
+    cube = new Cube(0, cubeSize)
+    cubes.push(cube)
+    let cubeCard = document.createElement("div")
+    document.getElementById("cubes").appendChild(cubeCard)
+    cubeCard.appendChild(cube.getHTMLCube())
+    cubeCard.appendChild(cube.getHTMLButtons())
   }
-  document.getElementById("solve").addEventListener("click", (e) => { 
+  document.getElementById("solve").addEventListener("click", (e) => {
     let maxMoves = document.getElementById("maxMoves").value
-    let side = document.getElementById("sideToSolve").value
+    let side = 0
     solver = new Solver()
-    solver.solveOneSide(cubes,side,maxMoves)
+    solver.solveOneSide(cubes, maxMoves)
   })
-  
+
 })
 
 
